85c7f4d779a795debf84d1773d98fcd78140eafe
Blake Thompson <bthompson@mmgyglobal.com>
2014-04-23 20:25:11 -0500

    running with NODE_ENV=production so livereload doesn't time out

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -21,9 +21,9 @@ module.exports = {
         callbackURL: 'http://localhost:3000/auth/github/callback'
     },
     google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
+        clientID: '475115761281.apps.googleusercontent.com',
+        clientSecret: 'hP3q_Ihc_pNhomIpY8RdAK_z',
+        callbackURL: 'http://truetiger.ape.arvixe.com:3000/auth/google/callback'
     },
     linkedin: {
         clientID: 'API_KEY',
